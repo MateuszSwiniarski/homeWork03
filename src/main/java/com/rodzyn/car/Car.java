f249cc498b92245e793320b76e701971b5a3d847
overment <mateusz.swiniarski@interia.pl>
2020-02-08 19:25:57 +0100

    homeWork03

---
+++ src/main/java/com/rodzyn/car/Car.java
@@ -0,0 +1,51 @@
+package com.rodzyn.car;
+
+public class Car {
+
+    private long id;
+    private String mark;
+    private String model;
+    private String color;
+
+    public Car() {
+    }
+
+    public Car(long id, String mark, String model, String color) {
+        this.id = id;
+        this.mark = mark;
+        this.model = model;
+        this.color = color;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/main/java/com/rodzyn/car/HomeWork03Application.java
@@ -0,0 +1,13 @@
+package com.rodzyn.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork03Application.class, args);
+    }
+
+}
