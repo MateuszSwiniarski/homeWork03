fe78ad72a41cd29f1077099da41ccbed00fedd86
overment <mateusz.swiniarski@interia.pl>
2020-02-13 20:11:42 +0100

    homeWork03 - change CarServiceImp for CarService

--- src/main/java/com/rodzyn/car/controller/CarApi.java
+++ src/main/java/com/rodzyn/car/controller/CarApi.java
@@ -1,6 +1,7 @@
 package com.rodzyn.car.controller;
 
         import com.rodzyn.car.model.Car;
+        import com.rodzyn.car.service.CarService;
         import com.rodzyn.car.service.CarServiceImp;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.hateoas.CollectionModel;
@@ -22,18 +23,18 @@ package com.rodzyn.car.controller;
 @RequestMapping("/cars")
 public class CarApi extends RepresentationModel<Car> {
 
-    private CarServiceImp carServiceImp;
+    private CarService carService;
 
     @Autowired
-    public CarApi(CarServiceImp carServiceImp) {
-        this.carServiceImp = carServiceImp;
+    public CarApi(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping(produces = {
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<CollectionModel<Car>> getCars(){
-        List<Car> cars = carServiceImp.getCars();
+        List<Car> cars = carService.getCars();
         cars.forEach(car -> car.add(linkTo(CarApi.class).slash(car.getId()).withSelfRel()));
         Link link = linkTo(CarApi.class).withSelfRel();
         CollectionModel<Car> carEntityModel = new CollectionModel(cars, link);
@@ -45,7 +46,7 @@ public class CarApi extends RepresentationModel<Car> {
             MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<EntityModel<Car>> getCarById(@PathVariable long id) {
         Link link = linkTo(CarApi.class).slash(id).withSelfRel();
-        Optional<Car> carById = carServiceImp.getCarById(id);
+        Optional<Car> carById = carService.getCarById(id);
         if (carById.isPresent()) {
             EntityModel<Car> carEntityModel = new EntityModel<>(carById.get(), link);
             return new ResponseEntity<>(carEntityModel, HttpStatus.OK);
@@ -57,7 +58,7 @@ public class CarApi extends RepresentationModel<Car> {
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<CollectionModel<Car>> getCarByColor(@RequestParam String color){
-        List<Car> carByColor = carServiceImp.getCarByColor(color);
+        List<Car> carByColor = carService.getCarByColor(color);
         carByColor.forEach(car -> car.add(linkTo(CarApi.class).slash(car.getId()).withSelfRel()));
         Link link = linkTo(CarApi.class).withSelfRel();
         CollectionModel<Car> carEntityModel = new CollectionModel(carByColor, link);
@@ -71,7 +72,7 @@ public class CarApi extends RepresentationModel<Car> {
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<CollectionModel<Car>> getCarByMark(@PathVariable String mark){
-        List<Car> carByMark = carServiceImp.getCarByMark(mark);
+        List<Car> carByMark = carService.getCarByMark(mark);
         carByMark.forEach(car -> car.add(linkTo(CarApi.class).slash(car.getId()).withSelfRel()));
         Link link = linkTo(CarApi.class).withSelfRel();
         CollectionModel<Car> carEntityModel = new CollectionModel(carByMark, link);
@@ -85,7 +86,7 @@ public class CarApi extends RepresentationModel<Car> {
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Car> addCar(@RequestBody Car car){
-        boolean add = carServiceImp.addCar(car);
+        boolean add = carService.addCar(car);
         if(add){
             return new ResponseEntity(HttpStatus.CREATED);
         }
@@ -96,10 +97,10 @@ public class CarApi extends RepresentationModel<Car> {
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Car> modCar(@RequestBody Car newCar){
-        Optional<Car> car = carServiceImp.modCar(newCar);
+        Optional<Car> car = carService.modCar(newCar);
         if(car.isPresent()){
-            carServiceImp.getCars().remove(car.get());
-            carServiceImp.addCar(newCar);
+            carService.getCars().remove(car.get());
+            carService.addCar(newCar);
             return new ResponseEntity<>(newCar, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -109,9 +110,9 @@ public class CarApi extends RepresentationModel<Car> {
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Car> removeCar(@PathVariable long id){
-        Optional<Car> first = carServiceImp.getCars().stream().filter(car -> car.getId() == id).findFirst();
+        Optional<Car> first = carService.getCars().stream().filter(car -> car.getId() == id).findFirst();
         if(first.isPresent()){
-            carServiceImp.removeCar(first.get());
+            carService.removeCar(first.get());
             return new ResponseEntity<>(first.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
